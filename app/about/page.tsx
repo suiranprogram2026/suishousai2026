2dbaf0da9a9c3331704c0e93a4847906c0916c7a
suiranprogram2026 <suiranprogram2026@gmail.com>
2026-03-07 17:46:27 +0900

    aboutの翠翔祭とは書きました。

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,24 @@
 
-export default function About() {
+import React from "react";
+import Header from "@/components/Header/Header";
+import "./about.css"
+import Image from 'next/image';
 
-}
+export default function About() {
+    return(
+        <div>
+            <div className="toppers">
+                <Header
+                    title="ABOUT"
+                    backgroundImage="/header/header-pc.png" 
+                    />
+            </div>
+            <div className="syousai">
+                <h1 className="midashi">
+                <p>翠翔祭とは</p>
+                </h1>
+                <p>翠翔祭とは江戸時代から伝統的に続いている豊作を願う祭りである。</p>
+            </div>
+        </div>
+)
+}   
