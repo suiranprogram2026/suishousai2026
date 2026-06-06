7892e8c09a925861644232d42bd0c70c81507620
suiranprogram2026 <suiranprogram2026@gmail.com>
2026-06-06 10:14:19 +0900

    カウントダウン

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             </div>
 
             <div className="left_date">
-              <Countdown targetDate="2026-06-26T23:59:59+09:00" /> {/*←翠翔祭の日程を入れる。数字は2桁*/}
+              <Countdown targetDate="2026-06-27T09:30:00+09:00" /> {/*←翠翔祭の日程を入れる。数字は2桁*/}
               {/*<Finish />*/}
             </div>
           </section>
