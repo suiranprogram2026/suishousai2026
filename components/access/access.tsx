b562c0c60cc2152432f0279cb02b6e24bcabaa7a
suiranprogram2026 <suiranprogram2026@gmail.com>
2026-06-08 01:15:41 +0900

    文字サイズ

--- components/access/access.tsx
+++ components/access/access.tsx
@@ -4,7 +4,6 @@ export default function Access() {
     return (
         <>
             <div className="access">
-                <h2 className="access_map">〈アクセスマップ〉</h2>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3249.2947119623113!2d139.60723897622412!3d35.47225144142478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60185c00a49b3861%3A0xf105a9d61ccd44fc!2z56We5aWI5bed55yM56uL5qiq5rWc57-g5bWQ6auY562J5a2m5qCh!5e0!3m2!1sja!2sjp!4v1744112148595!5m2!1sja!2sjp"
                     width="600"
