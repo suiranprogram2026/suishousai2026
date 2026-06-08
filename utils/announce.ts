b57ead8988dbad4035274e6be52daf93b774dbac
suiranprogram2026 <suiranprogram2026@gmail.com>
2026-06-09 08:57:31 +0900

    先生チェック

--- utils/announce.ts
+++ utils/announce.ts
@@ -6,8 +6,7 @@ export type AnnounceItem = {
 }
 
 export const announceItems: AnnounceItem[] = [
-  { id: 1, title: "重大イベント！", detail: "ここには様々なお知らせを記入していく予定です。当日は落とし物などもお知らせしようと思います。", date:"6/4" },
-  { id: 2, title: "翠翔祭ホームーページ開設！", detail: "2026翠翔祭-Luminous-のホームページが遂に開設しました！順次情報を公開予定です。首を長くしてお待ちください。", date:"5/18" },
+  { id: 2, title: "翠翔祭ホームーページ開設！", detail: "2026翠翔祭-Luminous-のホームページが遂に開設しました！順次情報を公開予定です。", date:"6/12" },
 ];
 
 
