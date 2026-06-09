a2c10639039e3497c0d405b222183c021266cf58
suiranprogram2026 <suiranprogram2026@gmail.com>
2026-06-09 23:31:07 +0900

    OPPOはゴミ（OPPOはスマホの縦が長いから今までの俺はOPPO対応にすぎなかった）

--- utils/announce.ts
+++ utils/announce.ts
@@ -6,7 +6,8 @@ export type AnnounceItem = {
 }
 
 export const announceItems: AnnounceItem[] = [
-  { id: 2, title: "翠翔祭ホームーページ開設！", detail: "2026翠翔祭-Luminous-のホームページが遂に開設しました！順次情報を公開予定です。", date:"6/12" },
+  { id: 2, title: "NEWSについて", detail: "ここでは準備期間から当日まで、翠翔祭に関する様々なお知らせを記載していきます。お楽しみに！", date:"6/12" },
+  { id: 1, title: "翠翔祭ホームーページ開設！", detail: "2026翠翔祭-Luminous-のホームページが遂に開設しました！順次情報を公開予定です。", date:"6/12" },
 ];
 
 
