ddff4bdc8f6d8a6cd8c53648b0d92d755e4fdc9f
junpiiii <zyunpei08kubozono@gmail.com>
2026-06-14 23:13:58 +0900

    suishou suisho

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://suishousai2026.vercel.app",
+  siteUrl: "https://suishosai2026.vercel.app",
   generateRobotsTxt: true,
 
   /** ↓ここから下までを消すとgoogle検索に乗せられる（すぐには乗らない）*/
